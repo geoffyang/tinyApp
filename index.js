b3c159863a98da87438c57a055b101acb420b055
geoffyang <geoffreyyang@gmail.com>
2021-09-14 18:07:38 -0400

    display navbar links;

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    sites=[ "app", "blog", "shop", "notes"]
+    sites = ["app", "blog", "shop", "notes"]
+
+    const navbar = document.getElementById('navbar')
+
+    for (site of sites) {
+        tempDiv = document.createElement('div')
+        tempDiv.className = "links";
+        tempDiv.text = site
+        navbar.appendChild(tempDiv)
+    }
+
 
 })
